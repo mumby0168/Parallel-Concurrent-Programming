28d3d89bbf8fdbf423a1d584f09abfe8fbf9a7eb
WILLIAM MUMBY <566334@hull.ac.uk>
2020-02-14 16:13:36 +0000

    simple vector dot product

---
+++ Labs/3/CUDA/CudaMemory/CudaMemory/kernel.cu
@@ -0,0 +1,147 @@
+
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+#include <iostream>
+
+
+#include <stdio.h>
+
+cudaError_t calcDotProductWithCuda(int *c, const int *a, const int *b, unsigned int size);
+
+
+
+
+int main()
+{
+    const int arraySize = 8;
+    const int a[arraySize] = { 2, 2, 2, 2, 2, 2, 2, 2 };
+    const int b[arraySize] = { 2, 2, 2, 2, 2, 2, 2, 2 };
+    int c[arraySize] = { 0 };
+
+    // Add vectors in parallel.
+    cudaError_t cudaStatus = calcDotProductWithCuda(c, a, b, arraySize);
+    if (cudaStatus != cudaSuccess) {
+        fprintf(stderr, "addWithCuda failed!");
+        return 1;
+    }
+
+	int result = c[0] + c[1];
+
+	std::cout << "Result of calculation:" << result << std::endl;
+
+    // cudaDeviceReset must be called before exiting in order for profiling and
+    // tracing tools such as Nsight and Visual Profiler to show complete traces.
+    cudaStatus = cudaDeviceReset();
+    if (cudaStatus != cudaSuccess) {
+        fprintf(stderr, "cudaDeviceReset failed!");
+        return 1;
+    }
+
+    return 0;
+}
+
+__shared__ int blockNumbers[4];
+
+__global__
+void calcDotProdKernel(int * c, const int *b, const int * a)
+{	
+	int i = blockIdx.x * blockDim.x + threadIdx.x;		
+
+	c[i] = a[i] * b[i];	
+	printf("ThreadId: %d BlockId: %d using index: %d Calculated: %d \n", threadIdx.x, blockIdx.x, i, c[i]);
+
+	blockNumbers[threadIdx.x] = c[i];
+	
+
+	//wait here until all threads reach this point inside the kernel
+	__syncthreads();
+
+
+	float subtotal = 0;
+	for (int k = 0; k < blockDim.x; k++)
+		subtotal += blockNumbers[k];	c[blockIdx.x] = subtotal;
+}
+
+
+
+
+
+// Helper function for using CUDA to add vectors in parallel.
+cudaError_t calcDotProductWithCuda(int *c, const int *a, const int *b, unsigned int size)
+{
+    int *dev_a = 0;
+    int *dev_b = 0;
+    int *dev_c = 0;
+    cudaError_t cudaStatus;
+
+    // Choose which GPU to run on, change this on a multi-GPU system.
+    cudaStatus = cudaSetDevice(0);
+    if (cudaStatus != cudaSuccess) {
+        fprintf(stderr, "cudaSetDevice failed!  Do you have a CUDA-capable GPU installed?");
+        goto Error;
+    }
+
+    // Allocate GPU buffers for three vectors (two input, one output)    .
+    cudaStatus = cudaMalloc((void**)&dev_c, size * sizeof(int));
+    if (cudaStatus != cudaSuccess) {
+        fprintf(stderr, "cudaMalloc failed!");
+        goto Error;
+    }
+
+    cudaStatus = cudaMalloc((void**)&dev_a, size * sizeof(int));
+    if (cudaStatus != cudaSuccess) {
+        fprintf(stderr, "cudaMalloc failed!");
+        goto Error;
+    }
+
+    cudaStatus = cudaMalloc((void**)&dev_b, size * sizeof(int));
+    if (cudaStatus != cudaSuccess) {
+        fprintf(stderr, "cudaMalloc failed!");
+        goto Error;
+    }
+
+    // Copy input vectors from host memory to GPU buffers.
+    cudaStatus = cudaMemcpy(dev_a, a, size * sizeof(int), cudaMemcpyHostToDevice);
+    if (cudaStatus != cudaSuccess) {
+        fprintf(stderr, "cudaMemcpy failed!");
+        goto Error;
+    }
+
+    cudaStatus = cudaMemcpy(dev_b, b, size * sizeof(int), cudaMemcpyHostToDevice);
+    if (cudaStatus != cudaSuccess) {
+        fprintf(stderr, "cudaMemcpy failed!");
+        goto Error;
+    }
+
+    // Launch a kernel on the GPU with one thread for each element.
+    calcDotProdKernel<<<2, 4>>>(dev_c, dev_b , dev_a);
+
+    // Check for any errors launching the kernel
+    cudaStatus = cudaGetLastError();
+    if (cudaStatus != cudaSuccess) {
+        fprintf(stderr, "addKernel launch failed: %s\n", cudaGetErrorString(cudaStatus));
+        goto Error;
+    }
+    
+    // cudaDeviceSynchronize waits for the kernel to finish, and returns
+    // any errors encountered during the launch.
+    cudaStatus = cudaDeviceSynchronize();
+    if (cudaStatus != cudaSuccess) {
+        fprintf(stderr, "cudaDeviceSynchronize returned error code %d after launching addKernel!\n", cudaStatus);
+        goto Error;
+    }
+
+    // Copy output vector from GPU buffer to host memory.
+    cudaStatus = cudaMemcpy(c, dev_c, size * sizeof(int), cudaMemcpyDeviceToHost);
+    if (cudaStatus != cudaSuccess) {
+        fprintf(stderr, "cudaMemcpy failed!");
+        goto Error;
+    }
+
+Error:
+    cudaFree(dev_c);
+    cudaFree(dev_a);
+    cudaFree(dev_b);
+    
+    return cudaStatus;
+}
